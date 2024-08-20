7fa43be4409039885c6c1633932d3d1d6059e095
tiago-kelis <tiagokelis2017@gmail.com>
2024-08-20 17:25:30 -0300

    gamer 1.1.2 -v

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ let largura = 0;
 let vidas = 1;
 let tempo = 15;
 
+let nivel = window.location.search;
+nivel = nivel.replace("?", "");
+
+let criarMoscaTempo = 1500;
+
+if(nivel === "normal") {
+
+    criarMoscaTempo = 1500;
+
+}else if(nivel === "dificil"){
+   
+    criarMoscaTempo = 1000;
+
+}else if(nivel === "bradock") {
+   
+    criarMoscaTempo = 550;
+}
+
+
+
 function ajusteTela () {
    
     largura = window.innerWidth;  
